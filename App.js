395d05973a4b34474bba8d6c5d906df94c7e0060
Lexi <has29@calvin.edu>
2023-10-09 18:16:13 -0400

    added comments and moved code around slightly

--- App.js
+++ App.js
@@ -21,91 +21,63 @@
  */
 
 import * as React from 'react';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import { useState, useRef, useEffect } from 'react';
 /* Import SelectList (Abigail) */
 import { SelectList } from 'react-native-dropdown-select-list';
-
-/* Import sound ability */
-import { Audio } from 'expo-av';
 
 /* Import component files */
 import Button from './components/Button';
 import BoxyBox from './components/BoxyBox';
 
-/* Default sound (this code is no longer used) */
-const DefaultClick = require('./assets/metronomesound.mp3'); // :) enjoy!
-const shotgun = require('./assets/shotgun.mp3');
+/* Import sound ability */
+import { Audio } from 'expo-av';
 
-/* Second sound (this code is no longer used) */
-const BackupClick = require('./assets/shotgun.mp3');
+
+/* Default sound and list of possible sounds*/
+const DefaultClick = require('./assets/metronomesound.mp3'); // :) enjoy!
+const soundList = [
+  { key: '1', value: 'Default' },
+  { key: '2', value: 'Bass' },
+  { key: '3', value: 'Clap' },
+  { key: '4', value: 'Piano' },
+  { key: '5', value: 'Shotgun' },
+]
 
 /* Main function */
 export default function App() {
   /* Hooks */
   const [pausePlayIcon, setPausePlayIcon] = useState("caretright")
+  const [selectedSound, setSelectedSound] = React.useState("Default"); // Initialize selected state with default sound
+  const [soundFile, setSoundFile] = useState(require('./assets/metronomesound.mp3'));
 
-  // Test (Abigail)
-  const [selected, setSelected] = React.useState("Default"); // Initialize selected state with default sound
+  /*hooks*/
+  var [BPM, setBPM] = useState(60);     //Beats per minute
+  var [beat, setBeat] = useState(4);    // how many beats are in a measure
+  var [count, setCount] = useState(0);  // which beat of the measure are we on
+  var [sound, setSound] = useState();   // sound that is currently loaded
+  var [isPlaying, setIsPlaying] = useState(false);  // is the metronome currently playing
 
-  // Create data "array" (Abigail)
-  const soundList = [
-    { key: '1', value: 'Default' },
-    { key: '2', value: 'Bass' },
-    { key: '3', value: 'Clap' },
-    { key: '4', value: 'Piano' },
-    { key: '5', value: 'Shotgun' },
-  ]
-  /*tempo stores the current tempo */
-  //These were set as constant variables, I dunno if var will fix all the probs, but now we can change them
-  var [tempo, setTempo] = useState(60);
-  var [beat, setBeat] = useState(4);
-  var [count, setCount] = useState(0);
-  var [sound, setSound] = useState();
-  var [isPlaying, setIsPlaying] = useState(false);
-
-  let metronomeInterval;
+  let metronomeInterval;  // time between each beat
 
   /* Toggles pause and play button. */
   const PausePlay = () => {
     if (!isPlaying) {
       setIsPlaying(true)
       setPausePlayIcon("pause");
-      setIsPlaying(true)
       playSound()
     } else {
       setIsPlaying(false)
       setPausePlayIcon("caretright");
       clearInterval(metronomeInterval); // Stop the metronome loop
-      setIsPlaying(false);              // set isPLaying to false
     }
   }
 
-  /* Redoing this function to see if it works to switch the sounds */
   /* Plays sound. The function is async playing an audio file is asynchronous. */
-  async function playSound(selectedSound) {
+  async function playSound() {
+    // Play sound, accenting the down beat
     console.log(count);
     setCount((count + 1) % beat);
-    // new
-    let soundFile;
-
-    switch (selected) {
-      case 'Bass':
-        soundFile = require('./assets/bass_c.mp3');
-        break;
-      case 'Clap':
-        soundFile = require('./assets/clap.mp3');
-        break;
-      case 'Piano':
-        soundFile = require('./assets/piano_c3.mp3');
-        break;
-      case 'Shotgun':
-        soundFile = require('./assets/shotgun.mp3');
-        break;
-      default:
-        soundFile = require('./assets/metronomesound.mp3'); // Default
-    }
-
     if (count == 0) {
       const { sound } = await Audio.Sound.createAsync(soundFile);
       setSound(sound);
@@ -115,25 +87,42 @@ export default function App() {
       setSound(sound);
       await sound.replayAsync();
     }
+    console.log(isPlaying);
   }
 
+  /*update the accent beat sound*/
+  useEffect(() => {
+    switch (selectedSound) {
+      case 'Bass':
+        setSoundFile(require('./assets/bass_c.mp3'));
+        break;
+      case 'Clap':
+        setSoundFile(require('./assets/clap.mp3'));
+        break;
+      case 'Piano':
+        setSoundFile(require('./assets/piano_c3.mp3'));
+        break;
+      case 'Shotgun':
+        setSoundFile(require('./assets/shotgun.mp3'));
+        break;
+      default:
+        setSoundFile(require('./assets/metronomesound.mp3')); // Default
+    }
+  },[selectedSound]);
 
-  /* The hook useEffect synchronizes a component with an external system. */
-  /* setInterval() implements the BPM */
+
+  /* setInterval() implements the metronome playing at a sepecific BPM */
   useEffect(() => {
 
     if (sound && isPlaying) {
-      let interval = (60 / tempo) * 1000;
+      let interval = (60 / BPM) * 1000;
 
       // Start the metronome loop
       metronomeInterval = setInterval(() => {
         playSound(); // Play the sound at the specified BPM interval
-      }, interval); // Tempo value works in reverse rn, higher=slower
+      }, interval); // BPM value works in reverse rn, higher=slower
     }
-
-    // Clean up when the component unmounts
-    // Is there a way to change this so it only unloads when it stops/is paused?
-    // I got the most slowdown when it kept having to load and unload the sound
+    
     return () => {
       console.log('Unloading sound');
       if (sound) {
@@ -141,7 +130,7 @@ export default function App() {
       }
       clearInterval(metronomeInterval);
     };
-  }, [sound, isPlaying, tempo]);
+  }, [sound, isPlaying, BPM]);
 
   /* Main app layout */
   return (
@@ -149,12 +138,12 @@ export default function App() {
       <View style={styles.header}>
         <Text style={styles.title}>Beatle</Text>
       </View>
-      <Button image={pausePlayIcon} onPress={PausePlay} w={250} h={100} />
+      <Button image={pausePlayIcon} onPress={PausePlay} w={250} h={100} /> 
       <View style={styles.updates}>
         <View style={styles.counters}>
           <View style={styles.boxed}>
             <Text style={styles.subtitle}> Tempo</Text>
-            <BoxyBox w={200} h={100} value={tempo} setValue={setTempo} min={20} max={200} />
+            <BoxyBox w={200} h={100} value={BPM} setValue={setBPM} min={20} max={200} />
           </View>
           <View style={styles.boxed}>
             <Text style={styles.subtitle}> Beat</Text>
@@ -164,7 +153,7 @@ export default function App() {
         <View style={styles.sounds}>
           <View style={styles.boxed}>
             <Text style={styles.subtitle}> Sound</Text>
-            <SelectList setSelected={(val) => setSelected(val)}
+            <SelectList setSelected={(val) => setSelectedSound(val)}
               data={soundList} save="value"
               boxStyles={{ backgroundColor: '#ff6900' }}
               dropdownTextStyles={{ color: '#ff6900' }}

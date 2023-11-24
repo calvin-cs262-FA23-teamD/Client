7ec9a31926c1d8e0662a1ca8dc985192b9238cbc
aam33 <113713028+aam33@users.noreply.github.com>
2023-11-24 18:05:44 -0500

    Revert "buttons can now be held down"

--- components/BoxyBox.js
+++ components/BoxyBox.js
@@ -4,7 +4,7 @@
 /* eslint-disable react/prop-types */
 
 /* eslint-disable import/no-extraneous-dependencies */
-/*import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 
 // app icons
 import { AntDesign } from '@expo/vector-icons';
@@ -30,7 +30,7 @@ export default function BoxyBox({
     <View style={[stylesMain.boxyBoxes, {
     }]}
     >
-      {/* <Text style={[stylesMain.text, { alignSelf: 'center' }]}>Tempo</Text> *//*}
+      {/* <Text style={[stylesMain.text, { alignSelf: 'center' }]}>Tempo</Text> */}
       <View style={{ width: w, height: h, alignItems: 'center', flexDirection: 'row' }}>
 
         <View style={stylesMain.plusMinusButtons}>
@@ -57,94 +57,5 @@ export default function BoxyBox({
 
       </View>
     </View>
-  );
-}
-
-*/
-
-import React, { useState, useEffect } from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
-
-import { stylesMain } from '../styles/styleMain';
-import { COLORS } from '../styles/colors';
-
-// app icons
-import { AntDesign } from '@expo/vector-icons';
-
-export default function BoxyBox({
-  w, h, value, setValue, max = 65, min = 55,
-}) {
-  const [incrementing, setIncrementing] = useState(false);
-  const [decrementing, setDecrementing] = useState(false);
-
-  useEffect(() => {
-    let interval;
-
-    const handleIncrement = () => {
-      if (value < max) {
-        setValue((prevValue) => prevValue + 1);
-      }
-    };
-
-    const handleDecrement = () => {
-      if (value > min) {
-        setValue((prevValue) => prevValue - 1);
-      }
-    };
-
-    if (incrementing) {
-      interval = setInterval(handleIncrement, 100);
-    }
-
-    if (decrementing) {
-      interval = setInterval(handleDecrement, 100);
-    }
-
-    return () => {
-      clearInterval(interval);
-    };
-  }, [value, max, min, incrementing, decrementing]);
-
-  const handlePressIn = (direction) => {
-    if (direction === 'increase') {
-      setIncrementing(true);
-      setDecrementing(false);
-    } else if (direction === 'decrease') {
-      setDecrementing(true);
-      setIncrementing(false);
-    }
-  };
-
-  const handlePressOut = () => {
-    setIncrementing(false);
-    setDecrementing(false);
-  };
-
-  return (
-    <View style={[stylesMain.boxyBoxes, { width: w, height: h, flexDirection: 'row', alignItems: 'center' }]}>
-      <View style={stylesMain.plusMinusButtons}>
-        <TouchableOpacity
-          style={{ padding: 10 }}
-          onPressIn={() => handlePressIn('decrease')}
-          onPressOut={handlePressOut}
-        >
-          {/* Trying to use minus from app icons (see import statements above) */}
-          <AntDesign name="minus" size={w / 10} color={COLORS.orange} />
-        </TouchableOpacity >
-      </View >
-      <View style={stylesMain.valueText}>
-        <Text style={{ color: COLORS.offWhite, fontSize: w / 6 }}>{value.toString()}</Text>
-      </View>
-      <View style={stylesMain.plusMinusButtons}>
-        <TouchableOpacity
-          style={{ padding: 10 }}
-          onPressIn={() => handlePressIn('increase')}
-          onPressOut={handlePressOut}
-        >
-          {/* Trying to use plus from app icons (see import statements above) */}
-          <AntDesign name="plus" size={w / 10} color={COLORS.orange} />
-        </TouchableOpacity>
-      </View>
-    </View >
   );
 }

0ce21e43e888429c25efa5551aff21f4d5e39cd3
Lexi <has29@calvin.edu>
2023-11-28 11:53:05 -0500

    grr

--- babel.config.js
+++ babel.config.js
@@ -2,14 +2,14 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: [
-      'babel-preset-expo', 
-      '@babel/preset-typescript', 
-      [ '@babel/preset-env',
-      {
-        targets: {
-          node: 'current'
-        }
-      }], 
+      'babel-preset-expo',
+      '@babel/preset-typescript',
+      ['@babel/preset-env',
+        {
+          targets: {
+            node: 'current',
+          },
+        }],
     ],
     plugins: [
       '@babel/plugin-proposal-export-namespace-from',
